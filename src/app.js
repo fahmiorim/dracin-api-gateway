cdd8b651f69e119c80cc23ad9684b470e82b2e86
fahmiorim <zulfahmi.mail@gmail.com>
2026-03-06 11:41:37 +0700

    fix: auto-populate API key from URL into Swagger UI authorization

--- src/app.js
+++ src/app.js
@@ -129,7 +129,20 @@ if (swaggerDocument) {
     swaggerOptions: {
       persistAuthorization: true,
       tryItOutEnabled: true
-    }
+    },
+    customJsStr: `
+      (function() {
+        var pollCount = 0;
+        var interval = setInterval(function() {
+          var apiKey = new URLSearchParams(window.location.search).get('api_key');
+          if (apiKey && window.ui) {
+            window.ui.preauthorizeApiKey('ApiKeyAuth', apiKey);
+            clearInterval(interval);
+          }
+          if (++pollCount > 100) clearInterval(interval);
+        }, 100);
+      })();
+    `
   }));
 
   app.get('/', (req, res) => {
